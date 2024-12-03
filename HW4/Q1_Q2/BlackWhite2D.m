53074a578e78c55670bc39e4d914270e73d075bd
ZSchreiber06 <104524110+ZSchreiber06@users.noreply.github.com>
2024-12-03 13:51:31 -0500

    Create BlackWhite2D.m

---
+++ HW4/Q1_Q2/BlackWhite2D.m
@@ -0,0 +1,56 @@
+classdef BlackWhite2D
+  properties (Access = private) % private members
+      image0
+  end 
+  properties % public memebers
+      mask
+  end
+  methods  % shifting main body of code into object
+      function obj = BlackWhite2D(img_int, mask_int)  % class constructor/initalizer
+          obj.image0 = img_int;
+          if(nargin < 2)
+              obj.mask = [0 1 0; 
+                          1 1 1; 
+                          0 1 0];
+          else 
+              obj.mask = mask_int;
+          end 
+      end 
+          
+      % Define set of functions associated with this object
+ 
+      function output = grow(self, iternum, mask_int)
+        if (nargin < 2)
+            iternum = 1;
+        
+        end 
+        if (nargin > 2)
+            self.mask = mask_int;
+        end 
+        for iter = 1:iternum
+            field0 = conv2(self.image0, self.mask, "same");
+            logic_field = logical(field0);
+            self.image0 = logic_field;
+            output = logic_field;
+        end
+        
+      end 
+      function output = shrink(self, iternum, mask_int)
+        if (nargin < 2)
+            iternum = 1;
+        end 
+        if (nargin > 2)
+            self.mask = mask_int;
+        end 
+        
+        for iter=1:iternum
+            field0 = conv2(self.image0, self.mask, "valid");
+            logic_field = logical(field0);
+            %self.image0 = logic_field;    % set initial condition
+            output = ~logic_field;
+        end
+      
+      end
+  
+    end 
+end
